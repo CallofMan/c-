f9f64502995a4a65a8082826851b9ebf514289bf
CallofMan <54943386+CallofMan@users.noreply.github.com>
2019-11-04 20:07:19 +0300

    addition of two real numbers

---
+++ resultTest2.1/resultTest2.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+using namespace std;
+
+double inputNumber()
+{
+	cout << "Enter a real number.\n";
+
+	double x;
+	cin >> x;
+
+	return x;
+}
+
+char inputSymbole()
+{
+	cout << "Enter -, +, * or /\n";
+
+	char x;
+	cin >> x;
+
+	return x;
+}
+
+int main()
+{
+	double number1 = inputNumber();
+	double number2 = inputNumber();
+	char symbole = inputSymbole();
+
+	double result;
+
+	if (symbole == '-') result = number1 - number2;
+	else if (symbole == '+') result = number1 + number2;
+	else if (symbole == '*') result = number1 * number2;
+	else if (symbole == '/') result = number1 / number2;
+
+	cout << number1 << " " << symbole << " " << number2 << " = " << result << endl;
+
+	return 0;
+}
