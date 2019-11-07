3a836fcefea9ec0bc235921d348acc83b584bd8f
CallofMan <54943386+CallofMan@users.noreply.github.com>
2019-11-07 16:54:17 +0300

    value numbers

---
+++ value numbers.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+int main()
+{
+	int outer = 1;
+	int before{ 4 };
+
+	while (outer <= 5)
+	{
+		int inner = outer;
+		int count{ 1 };
+
+		while (count <= before)
+		{
+			std::cout << ' ' << " ";
+			count++;
+		}
+
+		while (inner >= 1)
+			std::cout << inner-- << " ";
+
+		std::cout << "\n";
+
+		--before;
+		++outer;
+	}
+
+	return 0;
+}
