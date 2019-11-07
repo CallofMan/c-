b547d0446bab3280e128db5b1b3ba710de30b9f8
CallofMan <54943386+CallofMan@users.noreply.github.com>
2019-11-07 17:19:53 +0300

    value even numbers

---
+++ for even number.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int main()
+{
+	/*
+	for (int iii = 0; iii <= 20; ++iii)
+	{
+		
+		if (iii % 2 == 0)
+		{
+			std::cout << iii << std::endl;
+		}
+		
+	}
+	*/
+
+	for (int iii = 0; iii <= 20; iii += 2)
+	{
+		std::cout << iii << std::endl;
+	}
+
+	return 0;
+}
