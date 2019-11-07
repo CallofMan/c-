2f688a32c98b9e2e18ba49b7574b1ce0a7673355
CallofMan <54943386+CallofMan@users.noreply.github.com>
2019-11-07 17:33:55 +0300

    calculation factorial

---
+++ factorial.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+int calculationFactorial(int factorial)
+{
+	int result{ 1 };
+
+	for (int count{ 1 }; count <= factorial; ++count)
+	{
+		result *= count;
+	}
+
+	return result;
+}
+
+int main()
+{
+	using namespace std;
+
+	cout << "Enter a number for calculation his factorial: ";
+	int number;
+	cin >> number;
+
+	int factorial{ calculationFactorial(number) };
+
+	cout << "The factorial of " << number << " is " << factorial << endl;
+
+	return 0;
+}
+
