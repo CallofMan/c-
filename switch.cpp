48a3333a927d92d005d723863a25631225ebe5c1
CallofMan <54943386+CallofMan@users.noreply.github.com>
2019-11-07 15:38:31 +0300

    calculate with switch

---
+++ switch.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+int calculate(int x, int y, char sign)
+{
+	switch (sign)
+	{
+		case '+':
+			return (x + y);
+		case '-':
+			return (x - y);
+		case '*':
+			return (x * y);
+		case '/':
+			return (x / y);
+		case '%':
+			return (x % y);
+		default:
+			std::cout << "calculate(): Unhandled case\n";
+			return 0;
+	}
+}
+
+int main()
+{
+	using namespace std;
+
+	cout << "Enter a first integer number: ";
+	int x;
+	cin >> x;
+	cout << "Enter a second integer number: ";
+	int y;
+	cin >> y;
+	cout << "Enter a sign: ";
+	char sign;
+	cin >> sign;
+
+	cout << x << " " << sign << " " << y << " = " << calculate(x, y, sign) << endl;
+
+	return 0;
+}
