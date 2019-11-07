18b53fa27eb6a147c9cbb2e81b6ed990ed5dcb1c
CallofMan <54943386+CallofMan@users.noreply.github.com>
2019-11-07 15:59:10 +0300

    legs... legs... legs...

---
+++ Animals legs.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <string>
+
+enum class Animal
+{
+	PIG,
+	CHICKEN,
+	GOAT,
+	CAT,
+	DOG,
+	OSTRICH,
+};
+
+std::string getAnimalName(Animal animal)
+{
+	switch (animal)
+	{
+		case Animal::PIG:
+			return "pig";
+		case Animal::CAT:
+			return "cat";
+		case Animal::DOG:
+			return "dog";
+		case Animal::CHICKEN:
+			return "chicken";
+		case Animal::GOAT:
+			return "goat";
+		case Animal::OSTRICH:
+			return "ostrich";
+		default: 
+			return "unknown";
+	}
+}
+
+void printNumberOfLegs(Animal animal)
+{
+	using namespace std;
+
+	cout << "A " << getAnimalName(animal) << " has ";
+
+	switch (animal)
+	{
+		case Animal::CAT:
+		case Animal::DOG:
+		case Animal::GOAT:
+		case Animal::PIG:
+			cout << 4 << " legs." << endl;
+			break;
+		case Animal::CHICKEN:
+		case Animal::OSTRICH:
+			cout << 2 << " legs." << endl;
+			break;
+		default: cout << "unknown legs." << endl;
+	}
+}
+
+int main()
+{
+	printNumberOfLegs(Animal::PIG);
+	printNumberOfLegs(Animal::CHICKEN);
+
+	return 0;
+}
