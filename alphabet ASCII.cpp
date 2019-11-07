f7f9ee20c18a69a2b370148fa0990f762a24a403
CallofMan <54943386+CallofMan@users.noreply.github.com>
2019-11-07 16:23:57 +0300

    value alphabet

---
+++ alphabet ASCII.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main()
+{
+	char word{ 'a' };
+
+	while (word <= 'z')
+	{
+		std::cout << word << " - " << static_cast<int>(word) << std::endl;
+		word++;
+	}
+
+	return 0;
+}
